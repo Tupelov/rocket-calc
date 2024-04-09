40e75bdb52dfdcd2f7701b677fd13c7cfbba8e64
Christian Glaudin <15748552-Tupelov@users.noreply.gitlab.com>
2024-04-09 17:22:04 -0400

    Added Injector calculations

--- calc.m
+++ calc.m
@@ -4,15 +4,6 @@ clc
 rocket = yaml.loadFile("calc.yaml");
 
 
-s = yaml.dump(rocket)
-
-rocket.delatap = 240 * 6894.75729
-rocket.coef.p_lox = 1.141
-
-rocket.coef.p_ipa = 0.786
-
-
-
 while true
 
 x = input("Hello! Welcome to the rocket calc, please select an option below:" +newline + ...
@@ -20,70 +11,38 @@ x = input("Hello! Welcome to the rocket calc, please select an option below:" +n
     "2) Save" + newline+ ...
     "3) Mass Flow"+newline + ...
     "4) Display yaml"+newline+...
-    "5) Exit"+newline...
+    "5) Exit"+newline+...
+    "6) Calculate injector orifice area"+newline...
     );
     
 if x==1
- rocket   = yaml.loadFile("calc.yaml")
+ rocket   = yaml.loadFile("calc.yaml");
 end
-if x==2
-    yaml.dumpFile("calc.yaml",rocket)
-end
-if x==3
-    
- 
-rocket.mass_flow_ipa = rocket.mass_flow/(rocket.coef.of_ratio+1);
-rocket.mass_flow_lox = rocket.mass_flow - rocket.mass_flow_ipa;
 
+if x==2
+    yaml.dumpFile("calc.yaml",rocket);
+end
+
+if x==3
+    rocket.mass_flow_ipa = rocket.mass_flow/(rocket.coef.of_ratio+1);
+    rocket.mass_flow_lox = rocket.mass_flow - rocket.mass_flow_ipa;
 end
 if x==4
-    disp(yaml.dump(rocket))
+    disp(yaml.dump(rocket));
 end
 
 if x==5
-break
+    break
 end
+if x==6
+
+    rocket.a_lox = rocket.mass_flow_lox / (rocket.coef.cd * sqrt(2*rocket.coef.p_lox*rocket.delatap));
+    rocket.a_ipa = rocket.mass_flow_ipa / (rocket.coef.cd * sqrt(2*rocket.coef.p_ipa*rocket.delatap));
+
+    disp("LOx Orifice Area: "+rocket.a_lox*1000^2 + " mm^2")
+    disp("IPA Orifice Area: "+rocket.a_ipa*1000^2 + " mm^2")
+
+
 end
 
-%{
-
-fig = uifigure;
-
-
-
-
-
-
-
-save_button = uibutton(fig);
-quick_save = uibutton(fig);
-quick_load = uibutton(fig);
-
-save_button.Text = 'Save';
-quick_save.Text = 'Quick Save';
-quick_load.Text = 'Quick Load';
-
-
-save_button.ButtonPushedFcn = @test_press;
-quick_save.ButtonPushedFcn = @test_press;
-quick_load.ButtonPushedFcn = @test_press;
-
-
-btnX = 50;
-btnY = 50;
-btnWidth = 100;
-btnHeight = 22;
-save_button.Position = [btnX btnY btnWidth btnHeight];
-quick_save.Position = [btnX btnY+50 btnWidth btnHeight];
-quick_load.Position = [btnX btnY+100 btnWidth btnHeight];
-
-
-
-
-function test_press(app,src,event)
-    disp('This is a test!');
 end
-%}
-
-        
-
